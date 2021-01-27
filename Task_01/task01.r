650ac4f7bf59b738254c8b95a59b4f7a908ac2a5
Coleton Honaker <ch2048@mail.wvu.edu>
2021-01-27 17:26:04 -0500

    Cole Honaker Task 01 upload b

---
+++ Task_01/task01.r
@@ -0,0 +1,46 @@
+install.packages("swirl")
+library(swirl)
+ch2048
+1
+1
+1
+1
+1
+5 + 7
+1
+1
+1
+1
+x <- 5 + 7
+1
+x
+y <- x - 3
+y
+1
+z <- c(1.1, 9, 3.14)
+?c
+z
+c(z, 555, z)
+z * 2 + 100
+1
+1
+my_sqrt <- sqrt(z - 1)
+2
+my_sqrt
+1
+my_div <- z/my_sqrt
+3
+my_div
+1
+1
+1
+c(1, 2, 3, 4) +c(0, 10)
+1
+c(1, 2, 3, 4) + c(0, 10, 100)
+1
+1
+z * 2 + 1000
+1
+my_div
+2
+0
