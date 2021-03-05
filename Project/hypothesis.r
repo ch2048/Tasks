105e9e69d40c824cf904da855a78ba550c351e2b
Coleton Honaker <ch2048@mail.wvu.edu>
2021-03-05 15:49:28 -0500

    ch2048 Project

---
+++ Project/hypothesis.r
@@ -0,0 +1,4 @@
+setwd('C:\\Users\\Ch2048\\Desktop\\Evolution\\Tasks\\Project')
+Data <- read.csv('data.csv', stringsAsFactors=F)
+#Hypothesis:
+#Hip Dysplasia is more likely to occur in the German Shepherd breed than any other dog breed. 
