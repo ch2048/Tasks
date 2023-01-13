f2a74ab4650ae6b8fcc0066790591bb67a1d3479
ch2048 <ch2048@mix.wvu.edu>
2023-01-12 23:55:53 -0500

    ch2048 Task 01 upload b

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -1,3 +1,36 @@
 install.packages("swirl")
 library(swirl)
 Yes
+library(swirl)
+swirl()
+Cole
+1
+5 + 7
+1
+x <- 5 + 7
+1
+x
+y <- x - 3
+y
+1
+z <- c(1.1, 9, 3.14)
+?c
+z
+c(z, 555, z)
+z * 2 + 1000
+1
+my_sqrt <- sqrt(z - 1)
+1
+my_sqrt
+1
+my_div <- z / my_sqrt
+3
+my_div
+1
+c(1, 2, 3, 4) + c(0, 10)
+1
+c(1, 2, 3, 4) + c(0, 10, 100)
+1
+1
+my_div
+1
