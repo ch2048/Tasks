f52faa5ae57f095e7cac58be5373de16817c3745
ch2048 <ch2048@mix.wvu.edu>
2023-03-08 11:03:34 -0500

    Project

---
+++ Project/Code/Datapoints.r
@@ -0,0 +1,19 @@
+setwd('~/Desktop/Evolution/Tasks/Project/Data')
+Data1 <- read.csv("Saenkodata.csv")
+head (Data1)
+#The adult and larval melanic phenotypes do not sure the same loci at the Ba_CSAD gene. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
